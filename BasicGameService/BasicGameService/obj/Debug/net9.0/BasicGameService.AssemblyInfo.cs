aa9c2a79fbef16afa6ec9c5bca80dbee8b2d2531
ZribiMaram <maramzribi8@gmail.com>
2025-10-06 22:21:21 +0100

    Nettoyage

--- BasicGameService/BasicGameService/obj/Debug/net9.0/BasicGameService.AssemblyInfo.cs
+++ BasicGameService/BasicGameService/obj/Debug/net9.0/BasicGameService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BasicGameService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee533b1867bcae94ce64d22545e31f12fcefb3bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b38207354d2f197cf9fdb5bd4930c68b7a1b1532")]
 [assembly: System.Reflection.AssemblyProductAttribute("BasicGameService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BasicGameService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
